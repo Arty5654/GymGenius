1c6a1f53fe1d2ae609c6d63e5731f32eaaaa1ddc
Arty Avetissian <itsarty5654@gmail.com>
2023-06-05 21:54:27 -0400

    Initial Commit

---
+++ GymGenius/GymGeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymGeniusApp.swift
+//  GymGenius
+//
+//  Created by Arteom Avetissian on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GymGeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
